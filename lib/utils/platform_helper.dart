9d3e4abf43e0e8c96e7aafe3373a8df8b8cc6108
sylensa <sylensa.adolf@gmail.com>
2025-12-29 20:41:16 +0000

    minor changes

--- lib/utils/platform_helper.dart
+++ lib/utils/platform_helper.dart
@@ -2,8 +2,16 @@ import 'package:flutter/foundation.dart';
 
 class PlatformHelper {
   static bool get isWeb => kIsWeb;
-  static bool get isIOS =>
-      !kIsWeb && defaultTargetPlatform == TargetPlatform.iOS;
-  static bool get isAndroid =>
-      !kIsWeb && defaultTargetPlatform == TargetPlatform.android;
+
+  static bool get isAndroid => !kIsWeb && defaultTargetPlatform == TargetPlatform.android;
+
+  static bool get isIOS => !kIsWeb && defaultTargetPlatform == TargetPlatform.iOS;
+
+  static bool get isMobile => isAndroid || isIOS;
+
+  static bool get isDesktop => !kIsWeb && (
+    defaultTargetPlatform == TargetPlatform.macOS ||
+    defaultTargetPlatform == TargetPlatform.windows ||
+    defaultTargetPlatform == TargetPlatform.linux
+  );
 }
